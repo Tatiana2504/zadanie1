98d6aabcf3129e12cb26aefc904c96389ec766fd
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-16 17:09:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.metanit;
+
+class Variables {
+
+    public static void main (String[] args) {
+
+        double a = 3;
+
+        double b = 4;
+
+        double c ;
+
+        c = Math.sqrt (a* a + b* b);
+
+        System.out.println ("c = "+ c);
+
+    } }
