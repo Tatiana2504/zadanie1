148d7fff8a7d27955a079ca41fda091bf40e7361
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-27 00:04:56 +0300

    Cod15

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ package com.maker;
 public class Main {
 
     public static void main(String[] args) {
-        double d = 1e-305 * Math.PI;
-        System.out.print(d);
-        for (int i = 0; i < 4; i++)
-            System.out.print(d /= 100000);
+        System.out.print(1/2);
+        System.out.print(1/2.);
     }
 }
