63ad69f493bfd035e601e9683856b470cc9eaf40
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-27 00:06:15 +0300

    Cod16

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ package com.maker;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print(1/2);
-        System.out.print(1/2.);
+        int x=3;
+        int y=5;
+        System.out.print (x/y);
+        System.out.print((double)x/y);
+        System.out.print(1.0*x/y);
     }
 }
