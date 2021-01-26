4dd7acb773d8263e760bd0f2694c61bf6dfa5516
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-26 23:52:01 +0300

    Cod7

--- Main.java
+++ Main.java
@@ -3,14 +3,8 @@ package com.maker;
 public class Main {
 
     public static void main(String[] args) {
-        int i=300000;
+        System.out.print(1000*60*60*24*7);
 
-        System.out.print(i*i);
-
-        long m=i;
-
-        System.out.print(m*m);
-
-        System.out.print(1/(m-i));
+        System.out.print(1000*60*60*24*30);
     }
 }
