63ef764222fd4a73ce753ee7a3e4e2e4edece22d
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-26 23:47:53 +0300

    Cod5

--- Main.java
+++ Main.java
@@ -3,14 +3,8 @@ package com.maker;
 public class Main {
 
     public static void main(String[] args) {
-        byte x=5;
-        byte y1=x++;
-        byte y2=x--;
-        byte y3=++x;
-        byte y4=--x;
-        System.out.println(y1);
-        System.out.println(y2);
-        System.out.println(y3);
-        System.out.println(y4);
+        int x= 300000;
+
+        System.out.print(x*x);
     }
 }
