28d08c939831b9caaedba0b244d1bdc3175abc07
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-26 23:41:10 +0300

    Cod2

--- Main.java
+++ Main.java
@@ -3,9 +3,8 @@ package com.maker;
 public class Main {
 
     public static void main(String[] args) {
-        char symb1=1067;
-        char symb2 ='Ы';
-        System.out.println("symb1 contains "+ symb1);
-        System.out.println("symb2 contains "+ symb2);
+        char ch = 'J';
+        int intCh = (int) ch;
+        System.out.println("J corresponds with "+ intCh);
     }
 }
