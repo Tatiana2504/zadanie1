9d7aeba2f0ff19e684db4fb7a16e94d1608ebf45
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-27 00:01:43 +0300

    Cod13

--- Main.java
+++ Main.java
@@ -3,8 +3,15 @@ package com.maker;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print(1e20f*1e20f);
-
-        System.out.print(-1e200*1e200);
+        System.out.print(1e-40f/1e10f);
+        System.out.print(-1e-300/1e100);
+        float f=1e-6f;
+        System.out.print(f);
+        f+=0.002f;
+        System.out.print(f);
+        f+=3;
+        System.out.print(f);
+        f+=4000;
+        System.out.print(f);
     }
 }
