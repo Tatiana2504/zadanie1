4fefbf8f8e22e1925b66e62ff7ee20fecde47ac0
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-26 23:53:29 +0300

    Cod8

--- Main.java
+++ Main.java
@@ -3,8 +3,12 @@ package com.maker;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print(1000*60*60*24*7);
+        byte x=-128;
 
-        System.out.print(1000*60*60*24*30);
+        System.out.print(-x);
+
+        byte y=127;
+
+        System.out.print(++y);
     }
 }
