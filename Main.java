904baa7e6fc3e2d2db82f8fd1cc4af1f1ea09d8d
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-26 23:58:19 +0300

    Cod11

--- Main.java
+++ Main.java
@@ -3,8 +3,14 @@ package com.maker;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print(1+2+"text");
+        char c='A';
 
-        System.out.print("text"+1+2);
+        System.out.print(c);
+
+        System.out.print(c+1);
+
+        System.out.print("c="+c);
+
+        System.out.print('c' + '=' + c);
     }
 }
