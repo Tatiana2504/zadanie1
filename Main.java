07ada1b73f9b190ba98ae971231b1f945254a4f7
Tatiana2504 <77538034+Tatiana2504@users.noreply.github.com>
2021-01-27 00:12:05 +0300

    Cod18

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ package com.maker;
 public class Main {
 
     public static void main(String[] args) {
-        long l=111111111111L;
-        float f = l;
-        l = (long) f;
-        System.out.print(l);
-    }
-}
+        String s = "abc";
+        Class cl=s.getClass();
+        System.out.println(cl.getName());
+}}
